c93bebb85ee810e055349f508f9142bdfae49fd5
abhay-gooru <abhay@gooru.org>
2018-09-18 17:34:48 +0530

    - Added impl to navigatge to student.self.report  with notification

--- app/components/notification/notification-indicator.js
+++ app/components/notification/notification-indicator.js
@@ -184,10 +184,15 @@ export default Ember.Component.extend({
           navigate: true,
           navigationDetails: {
             route: 'teacher.class.course-map',
-            queryPType: 'paramonly',
+            queryPType: 'hybrid',
             exactparams: 'classId',
+            setlocation: true,
             queryparams: {
-              classId: 0
+              classId: 0,
+              unitId: 0,
+              lessonId: 0,
+              collectionId: 0,
+              location: 'unitId+lessonId+collectionId+currentItemType'
             }
           }
         }
