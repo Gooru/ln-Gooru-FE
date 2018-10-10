4b0a8d90af6b04c5e690bbd024d45c73fbfde44e
sheeban <sheeban@gooru.org>
2018-10-10 18:55:31 +0530

    NILE-3285: DCA - Teacher: Schedule  DCA content from search or course map should bring up datepicker

--- app/components/cards/dca-search-collection-card.js
+++ app/components/cards/dca-search-collection-card.js
@@ -9,12 +9,22 @@ export default Ember.Component.extend({
   classNames: ['cards', 'dca-search-collection-card'],
 
   // -------------------------------------------------------------------------
+  // Dependencies
+
+  /**
+   * @requires service:api-sdk/class-activity
+   */
+  classActivityService: Ember.inject.service('api-sdk/class-activity'),
+
+  //
+  // -------------------------------------------------------------------------
   // Events
   didRender() {
-    var component = this;
+    let component = this;
     component.$('[data-toggle="tooltip"]').tooltip({
       trigger: 'hover'
     });
+    component.setupDatepicker();
   },
 
   // -------------------------------------------------------------------------
@@ -46,6 +56,24 @@ export default Ember.Component.extend({
    */
   contentType: null,
 
+  /**
+   * Maximum number of days to schedule dca content ahead.
+   * @type {Number}
+   */
+  maxNumberOfDays: 30,
+
+  /**
+   * Selected collection for scheduling
+   * @type {Object}
+   */
+  selectedCollectionForSchedule: null,
+
+  /**
+   * Class Id
+   * @type {String}
+   */
+  classId: null,
+
   // -------------------------------------------------------------------------
   // Actions
 
@@ -71,8 +99,65 @@ export default Ember.Component.extend({
      * Action triggered when schedule collection to dca.
      * @param  {Object} collection
      */
-    onScheduleCollectionToDCA(collection) {
-      this.sendAction('onScheduleContentToDCA', collection);
+    onScheduleCollectionToDCA(collection, event) {
+      let element = this.$(event.target).find('.schedule-dca-datepicker');
+      if (!element.hasClass('active')) {
+        this.$('.schedule-dca-datepicker').removeClass('active');
+        element.addClass('active').datepicker('show');
+      } else {
+        element.removeClass('active').datepicker('hide');
+      }
+      this.set('selectedCollectionForSchedule', collection);
     }
+  },
+
+  // -------------------------------------------------------------------------
+  // Methods
+
+  setupDatepicker() {
+    let component = this;
+    let startDate = moment().toDate();
+    let maxNumberOfDays = component.get('maxNumberOfDays');
+    let endDate = moment()
+      .add(maxNumberOfDays, 'd')
+      .toDate();
+    component.$('.schedule-dca-datepicker').datepicker({
+      startDate: startDate,
+      endDate: endDate,
+      format: 'yyyy-mm-dd',
+      maxViewMode: 0,
+      orientation: 'bottom right',
+      container: '.teacher-class-search-content-pull-up'
+    });
+    component
+      .$('.schedule-dca-datepicker')
+      .off('changeDate')
+      .on('changeDate', function() {
+        let datepicker = this;
+        let scheduleDate = component
+          .$(datepicker)
+          .datepicker('getFormattedDate')
+          .valueOf();
+        let classId = component.get('classId');
+        let contentType = component.get('contentType');
+        let contentId = component.get('selectedCollectionForSchedule.id');
+        component
+          .$(datepicker)
+          .removeClass('active')
+          .datepicker('hide');
+        component
+          .get('classActivityService')
+          .addActivityToClass(classId, contentId, contentType, scheduleDate)
+          .then(newContentId => {
+            if (!component.isDestroyed) {
+              component.sendAction(
+                'addedScheduleContentToDCA',
+                component.get('selectedCollectionForSchedule'),
+                newContentId,
+                scheduleDate
+              );
+            }
+          });
+      });
   }
 });
