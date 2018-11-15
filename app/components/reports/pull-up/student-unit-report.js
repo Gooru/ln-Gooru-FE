2956382b01824a73f48cbef1a625ef5fb89e8cd7
Gugan <gugankumar.p@bambeeq.com>
2018-11-15 12:27:40 +0530

    updated student course report,student lesson report,student unit report pull-ups

--- app/components/reports/pull-up/student-unit-report.js
+++ app/components/reports/pull-up/student-unit-report.js
@@ -110,7 +110,12 @@ export default Ember.Component.extend({
 
   // -------------------------------------------------------------------------
   // Events
-
+  didRender() {
+    this.handleAppContainerScroll();
+  },
+   didDestroyElement() {
+    this.handleAppContainerScroll();
+  },
   /**
    * Function to triggered once when the component element is first rendered.
    */
@@ -256,7 +261,15 @@ export default Ember.Component.extend({
       }
     );
   },
-
+  handleAppContainerScroll() {
+    let activePullUpCount = Ember.$(document.body).find('.backdrop-pull-ups')
+      .length;
+    if (activePullUpCount > 0) {
+      Ember.$(document.body).addClass('no-vertical-scroll');
+    } else if (activePullUpCount === 0) {
+      Ember.$(document.body).removeClass('no-vertical-scroll');
+    }
+  },
   handleScrollToFixHeader() {
     let component = this;
     component
