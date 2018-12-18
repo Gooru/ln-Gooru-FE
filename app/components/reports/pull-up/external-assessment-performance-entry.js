ad4efd320c6e1ebcf75c470c557ded841c24cf3b
Gugan <gugankumar.p@bambeeq.com>
2018-12-18 10:37:07 +0530

    Rendered students list in the external assessment entry screen

--- app/components/reports/pull-up/external-assessment-performance-entry.js
+++ app/components/reports/pull-up/external-assessment-performance-entry.js
@@ -8,5 +8,21 @@ export default Ember.Component.extend({
     let component = this;
     console.log('assessment', this.get('assessment'));
     console.log('students', this.get('students'));
-  }
+  },
+
+  actions: {
+    onSelectStudent(student, studentSeq) {
+
+    },
+
+    onEditScore(studentSeq) {
+
+    }
+  },
+
+  activeStudent: Ember.computed('students', function() {
+    let component = this;
+    let students = component.get('students');
+    return students ? students.objectAt(0) : null;
+  })
 });
