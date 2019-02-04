2aa0256f915e0cd0f821f1c52d8f81f3eafd89eb
Gugan <gugankumar.p@bambeeq.com>
2019-02-04 17:42:02 +0530

    added signature content in teacher

--- app/routes/student-learner-proficiency.js
+++ app/routes/student-learner-proficiency.js
@@ -7,6 +7,9 @@ export default Ember.Route.extend({
     },
     courseId: {
       refreshModel: true
+    },
+    isTeacher: {
+      refreshModel: true
     }
   },
   // -------------------------------------------------------------------------
@@ -45,6 +48,7 @@ export default Ember.Route.extend({
     let studentId = params.userId;
     const classId = params.classId;
     const courseId = params.courseId;
+    const isTeacher = params.isTeacher ? params.isTeacher : false;
     return Ember.RSVP.hash({
       profilePromise: route.get('profileService').readUserProfile(studentId),
       classPromise: route.get('classService').readClassInfo(classId),
@@ -59,7 +63,8 @@ export default Ember.Route.extend({
         profile: studentProfile,
         categories: taxonomyCategories,
         class: aClass,
-        course: course
+        course: course,
+        isTeacher: isTeacher
       });
     });
   },
@@ -67,12 +72,9 @@ export default Ember.Route.extend({
   setupController(controller, model) {
     controller.set('studentProfile', model.get('profile'));
     controller.set('class', model.get('class'));
+    controller.set('isTeacher', model.get('isTeacher'));
     controller.set('course', model.get('course'));
     controller.set('taxonomyCategories', model.get('categories'));
     controller.loadData();
-  },
-  resetController(controller) {
-    controller.set('showDomainInfo', false);
-    controller.set('showCompetencyInfo', false);
   }
 });
