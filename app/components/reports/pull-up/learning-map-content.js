5d86620c1f0bfa08127494badba890f6de0b3b4d
Ajay <ajaykumar>
2019-02-14 11:24:07 +0530

    Learning map for teacher

---
+++ app/components/reports/pull-up/learning-map-content.js
@@ -0,0 +1,7 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  // -------------------------------------------------------------------------
+  // Attributes
+  classNames: ['learning-map-content']
+});
