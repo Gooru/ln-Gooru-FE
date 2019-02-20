130c87df938fb1c1cc3a3ddfb9cafe0d82775a19
abhay-gooru <abhay@gooru.org>
2019-02-20 12:11:04 +0530

    Resolved conflicts

--- app/components/teacher/class/atc-chart.js
+++ app/components/teacher/class/atc-chart.js
@@ -1,12 +1,6 @@
 import Ember from 'ember';
 import d3 from 'd3';
-<<<<<<< HEAD
 import { getGradeColor } from 'gooru-web/utils/utils';
-=======
-import {
-  getGradeColor
-} from 'gooru-web/utils/utils';
->>>>>>> Extract subject code from preference
 
 export default Ember.Component.extend({
   // -------------------------------------------------------------------------
