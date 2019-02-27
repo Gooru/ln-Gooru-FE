c94287083cb67b864b317e6c5111bd0131494b58
Gugan <gugankumar.p@bambeeq.com>
2019-02-27 17:16:09 +0530

    Renamed component

--- app/components/cards/ca-content-list.js
+++ app/components/cards/ca-content-list.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 
-  classNames: ['ca-activity-list'],
+  classNames: ['ca-content-list'],
 
   // -------------------------------------------------------------------------
   // Dependencies
