03b15e2cbe3355ed11217aa3ad3801179aa98d6f
Ajaykumar <ajaykumar.s@bambeeq.com>
2019-03-29 16:05:33 +0530

    Fixed redbar issue

--- app/components/search/search-by-filter.js
+++ app/components/search/search-by-filter.js
@@ -69,7 +69,7 @@ export default Ember.Component.extend({
    */
   onSubjectSelected: Ember.observer('selectedSubject.courses.[]', function() {
     let component = this;
-    component.set('selectedCourse', this.get('selectedSubject.courses'));
+    component.set('selectedCourse', component.get('selectedSubject.courses'));
     component.loadTaxonomyPicker();
   }),
 
@@ -168,6 +168,7 @@ export default Ember.Component.extend({
 
     selectSubject(subject) {
       let component = this;
+      component.set('selectedSubject.courses', Ember.A([]));
       component.set('selectedSubject', subject);
       let selectedFilters = component.get('selectedFilters');
       selectedFilters.removeObjects(selectedFilters.filterBy('filter', 'flt.standard')); //remove previous object
@@ -194,10 +195,10 @@ export default Ember.Component.extend({
     setPublisher() {
       let component = this;
       let term = this.get('publisherName').trim();
-      let filterItems = component.get('selectedFilters');
-      filterItems.removeObjects(filterItems.filterBy('filter', 'flt.publisherName')); //remove previous object
+      let selectedFilters = component.get('selectedFilters');
+      selectedFilters.removeObjects(selectedFilters.filterBy('filter', 'flt.publisherName')); //remove previous object
       if (term !== '') {
-        filterItems.push(Ember.Object.create({
+        component.get('selectedFilters').pushObject(Ember.Object.create({
           'filter': 'flt.publisherName',
           'name': term
         }));
