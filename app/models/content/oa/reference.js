6d058fb77d4a83a4b9449accdf6937ad31984a12
abhay-gooru <abhay@gooru.org>
2019-06-13 22:39:36 +0530

    - Added url validations

--- app/models/content/oa/reference.js
+++ app/models/content/oa/reference.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
+import { validator, buildValidations } from 'ember-cp-validations';
+
+const Validations = buildValidations({
+  location: [
+    validator('presence', {
+      presence: true,
+      message: '{{description}}',
+      descriptionKey: 'common.enter-url'
+    }),
+    validator('format', {
+      type: 'url',
+      message: '{{description}}',
+      descriptionKey: 'errors.url'
+    })
+  ]
+});
+
 /**
  *
  *
@@ -6,75 +23,73 @@ import Ember from 'ember';
  *
  * @typedef {Object} Content/oa/reference
  */
-export default Ember.Object.extend(
-  /* Validations, */ {
-    /**
-     * @property {String} id - Gooru id for the unit
-     */
-    id: null,
+export default Ember.Object.extend(Validations, {
+  /**
+   * @property {String} id - Gooru id for the unit
+   */
+  id: null,
 
-    /**
-     * @property {String} id- Parent OA id
-     */
-    oaId: null,
+  /**
+   * @property {String} id- Parent OA id
+   */
+  oaId: null,
 
-    /**
-     * @property {String} type
-     */
-    type: '',
+  /**
+   * @property {String} type
+   */
+  type: '',
 
-    /**
-     * @property {String} subType
-     */
-    subType: '',
+  /**
+   * @property {String} subType
+   */
+  subType: '',
 
-    /**
-     * @property {location} location
-     */
-    location: '',
+  /**
+   * @property {location} location
+   */
+  location: null,
 
-    /**
-     *
-     *
-     * @function
-     * @return {Content/oa/task}
-     */
-    copy: function() {
-      var properties = [];
-      var enumerableKeys = Object.keys(this);
+  /**
+   *
+   *
+   * @function
+   * @return {Content/oa/task}
+   */
+  copy: function() {
+    var properties = [];
+    var enumerableKeys = Object.keys(this);
 
-      for (let i = 0; i < enumerableKeys.length; i++) {
-        let key = enumerableKeys[i];
-        let value = Ember.typeOf(this.get(key));
+    for (let i = 0; i < enumerableKeys.length; i++) {
+      let key = enumerableKeys[i];
+      let value = Ember.typeOf(this.get(key));
 
-        // Copy null values as well to avoid triggering the validation on empty input fields
-        if (
-          value === 'string' ||
-          value === 'number' ||
-          value === 'boolean' ||
-          value === 'null'
-        ) {
-          properties.push(key);
-        }
+      // Copy null values as well to avoid triggering the validation on empty input fields
+      if (
+        value === 'string' ||
+        value === 'number' ||
+        value === 'boolean' ||
+        value === 'null'
+      ) {
+        properties.push(key);
       }
-
-      // Copy the task data
-      properties = this.getProperties(properties);
-
-      return this.get('constructor').create(properties);
-    },
-
-    /**
-     * Copy a list of property values from another reference to override the current ones
-     *
-     * @function
-     * @param {Content/oa/reference} reference
-     * @param {String[]} propertyList
-     * @return {null}
-     */
-    merge: function(unit, propertyList = []) {
-      var properties = unit.getProperties(propertyList);
-      this.setProperties(properties);
     }
+
+    // Copy the task data
+    properties = this.getProperties(properties);
+
+    return this.get('constructor').create(properties);
+  },
+
+  /**
+   * Copy a list of property values from another reference to override the current ones
+   *
+   * @function
+   * @param {Content/oa/reference} reference
+   * @param {String[]} propertyList
+   * @return {null}
+   */
+  merge: function(unit, propertyList = []) {
+    var properties = unit.getProperties(propertyList);
+    this.setProperties(properties);
   }
-);
+});
