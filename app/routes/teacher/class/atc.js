f24d9f8e59ff75291038b3e385497c99f4ef8f2a
Ajaykumar <ajaykumar.s@bambeeq.com>
2019-08-05 16:05:05 +0530

    text is not visible in atc and add zoom option

--- app/routes/teacher/class/atc.js
+++ app/routes/teacher/class/atc.js
@@ -1,6 +1,25 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  //
+  // Actions
+  actions: {
+    didTransition() {
+      let $viewport = Ember.$('head meta[name="viewport"]');
+      $viewport.attr(
+        'content',
+        'width=device-width, initial-scale=1, maximum-scale=4'
+      );
+    },
+    willTransition() {
+      let $viewport = Ember.$('head meta[name="viewport"]');
+      $viewport.attr(
+        'content',
+        'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no'
+      );
+    }
+  },
+
   setupController(controller) {
     controller.get('classController').selectMenuItem('atc');
     controller.initializeController();
