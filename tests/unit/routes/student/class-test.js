dc54d57bf8f893f667d0a88a44092d323fe0e149
Ajaykumar <ajaykumar.s@bambeeq.com>
2019-09-16 16:40:56 +0530

    fixed dependency service injection

--- tests/unit/routes/student/class-test.js
+++ tests/unit/routes/student/class-test.js
@@ -3,7 +3,7 @@ import { test } from 'ember-qunit';
 
 moduleForComponent('route:student/class', 'Unit | Route | student/class', {
   // Specify the other units that are required for this test.
-  needs: ['service/taxonomy']
+  needs: ['service:taxonomy']
 });
 
 test('it exists', function(assert) {
