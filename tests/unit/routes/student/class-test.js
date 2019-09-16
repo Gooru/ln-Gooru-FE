364c4af79766c7147fd3122716fdc21c382300a2
Ajaykumar <ajaykumar.s@bambeeq.com>
2019-09-16 16:07:18 +0530

    Added dependency service injection

--- tests/unit/routes/student/class-test.js
+++ tests/unit/routes/student/class-test.js
@@ -3,17 +3,7 @@ import { test } from 'ember-qunit';
 
 moduleForComponent('route:student/class', 'Unit | Route | student/class', {
   // Specify the other units that are required for this test.
-  /* needs: [
-    'service:api-sdk/authentication',
-    'service:api-sdk/session',
-    'service:api-sdk/class',
-    'service:api-sdk/performance',
-    'service:api-sdk/course',
-    'service:api-sdk/unit',
-    'service:api-sdk/navigate-map',
-    'service:api-sdk/skyline-initial',
-    'service:api-sdk/competency'
-  ] */
+  needs: ['service/taxonomy']
 });
 
 test('it exists', function(assert) {
