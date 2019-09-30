2668907aeb8010d6a3bcc184ad2ad0d4f020fda5
arunkumar <arunbalan005@gmail.com>
2019-10-01 01:45:21 +0530

    Resolved conflict

--- app/components/reports/pull-up/learning-map-content.js
+++ app/components/reports/pull-up/learning-map-content.js
@@ -2,6 +2,13 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   // -------------------------------------------------------------------------
+  // Dependencies
+  /**
+   * @property {service} searchService
+   */
+  searchService: Ember.inject.service('api-sdk/search'),
+
+  // -------------------------------------------------------------------------
   // Attributes
   classNames: ['learning-map-content'],
 
@@ -32,14 +39,22 @@ export default Ember.Component.extend({
       component.loadActivityContentData(contentType);
       component.set('isShowActivityPullup', true);
     },
+
     onClickShowMoreActivity() {
       let component = this;
       component.loadActivityContentData();
+    },
+
+    onResetPullUpData() {
+      let component = this;
+      component.resetActivityContentData();
     }
   },
   loadActivityContentData(contentType) {
     let component = this;
     component.set('isLoading', true);
+    let searchService = component.get('searchService');
+    let competencyCode = component.get('learningMapData.gutCode');
     let activityContentType =
       contentType || component.get('activityContentType');
     let activityContents = component.get('activityContents');
@@ -52,12 +67,48 @@ export default Ember.Component.extend({
     let activityContentData = learningMapData
       ? learningMapData.learningMapsContent[`${activityContentType}`]
       : Ember.A([]);
-    component.set(
-      'activityContents',
-      activityContents.concat(activityContentData)
-    );
+    if (startAt) {
+      let filters = {
+        startAt: component.get('startAt'),
+        length: 5,
+        isCrosswalk: false
+      };
+      Ember.RSVP.hash({
+        loadMoreLearningMapData: searchService.fetchLearningMapsCompetencyContent(
+          competencyCode,
+          filters
+        )
+      }).then(({ loadMoreLearningMapData }) => {
+        component.set(
+          'activityContents',
+          activityContents.concat(
+            loadMoreLearningMapData.learningMapsContent[
+              `${activityContentType}`
+            ]
+          )
+        );
+      });
+    } else {
+      component.set(
+        'activityContents',
+        activityContents.concat(activityContentData)
+      );
+    }
     component.set('activityTotalHitCount', activityTotalHitCount);
     component.set('startAt', startAt + length);
     component.set('isLoading', false);
+  },
+
+  /**
+   * @function resetActivityContentData
+   * Method to reset activity content data to default
+   */
+  resetActivityContentData() {
+    let component = this;
+    component.set('activityContents', Ember.A([]));
+    component.set('startAt', 0);
+    component.set('isShowActivityPullup', false);
+    component.set('activityContentType', '');
+    component.set('activityTotalHitCount', 0);
   }
 });
