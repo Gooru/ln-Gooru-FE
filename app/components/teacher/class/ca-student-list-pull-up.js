8f44ec96108b465e303dbab9a66bb4c8111bc254
arunkumar <arunbalan005@gmail.com>
2020-01-29 10:28:34 +0530

    added order by in student list

--- app/components/teacher/class/ca-student-list-pull-up.js
+++ app/components/teacher/class/ca-student-list-pull-up.js
@@ -82,7 +82,10 @@ export default Ember.Component.extend({
       let component = this;
       let studentSearchText = component.get('studentSearchText');
       if (!studentSearchText) {
-        return component.get('students');
+        return component
+          .get('students')
+          .sortBy('isSelected')
+          .reverse();
       }
       let regexp = new RegExp(studentSearchText, 'gi');
       return component.get('students').filter(function(student) {
