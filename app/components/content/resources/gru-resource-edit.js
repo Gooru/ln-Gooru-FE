216a051b9d8517ce57d79435a85e594afdc45e71
Eniya Ravanan <eniya.r@bambeeq.com>
2020-01-30 12:52:41 +0530

    Fixed the build error and check the resource is original or copy.

--- app/components/content/resources/gru-resource-edit.js
+++ app/components/content/resources/gru-resource-edit.js
@@ -457,8 +457,10 @@ export default Ember.Component.extend(
 
     isEditResources: Ember.computed('resource', function() {
       let resourceOwner = this.get('resource.owner');
+      let OwnerId = resourceOwner ? resourceOwner.get('id') : null;
       let userId = this.get('session.userId');
-      return userId === resourceOwner.get('id');
+      let originalResourceId = this.get('resource.originalResourceId');
+      return !originalResourceId && userId === OwnerId;
     }),
 
     // -------------------------------------------------------------------------
